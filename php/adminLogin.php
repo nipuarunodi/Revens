53938f9d25bde0a4ccdd2dff3539bb06e6b698b8
MadhushaSiriwardane <madhushasiriwardane@gmail.com>
2021-10-03 17:48:22 +0530

    admin login page created by Madhusha

---
+++ php/adminLogin.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Admin Login</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post">
+  <table align="center" width="417" height="295" border="1">
+    <tbody>
+      <tr>
+        <td height="53" colspan="2" align="center"><h1>ADMIN LOGIN</h1></td>
+      </tr>
+      <tr>
+        <td width="146" height="91"><h2>Email</h2></td>
+        <td width="257"><input name="txtEmail" type="text" required="required" id="textfield"></td>
+      </tr>
+      <tr>
+        <td height="93"><h2>Password</h2></td>
+        <td><p>
+          <input name="password" type="password" required="required" id="password">
+        </p></td>
+      </tr>
+      <tr>
+        <td height="41"><center><p>&nbsp;</p></center></td>
+        <td><input type="submit" name="submit" id="" value="Login">  <input type="reset" name="btnreset" id="btnreset" value="Reset"></td>
+      </tr>
+    </tbody>
+  </table>
+	  <?php
+				if(isset($_POST["submit"]))
+				{
+			  		$username = "admin"; 
+			  		$password = "admin";
+					
+					if(($username==$_POST["txtEmail"])&&($password==$_POST["password"]))
+					{
+						header('Location:AdminProfile.php');
+					}
+					else
+					{
+						echo"Please enter correct username and password";
+					}
+				}
+	  ?>
+</body>
+</html>
