3ce0fa6794671adb2225c368ff1c3cd75df11fb2
shehaniliyanage <it20760656@my.sliit.lk>
2021-10-29 23:02:25 +0530

    Modified StockManagement.php by shehani

--- php/StockManagement.php
+++ php/StockManagement.php
@@ -35,47 +35,59 @@ include "config.php";
 <!DOCTYPE html>
 <html lang="en">
   <head>
-      <meta charset="utf-8">
-	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	  <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Stock Management</title>  
-  </head>
-  <body>
-        <h2><center style="font-weight: inherit; font-size: 36px;">
-        <p style="color: #000000">Manage Stocks</p></center></h2>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Stock Management</title>
+	  
+    <link href="../css/bootstrap-4.3.1.css" rel="stylesheet">
+	<script src="../js/jquery-3.3.1.min.js"></script>
+	<script src="../js/popper.min.js"></script> 
+	<script src="../js/bootstrap-4.3.1.js"></script>
+   </head>
+  <body>   
+  <h2><center>
+  <p style="color: #000000; font-size: 50px; font-family: Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif;">Manage Stocks</p>
+  </center></h2>
         <form action="StockManagement.php" method="POST">
         <p><br></p>
-	  
-        <table width="462" height="307" border="0" align="center">
+	     <table width="462" height="305" border="0" align="center">
         <tbody>
         <tr>
-          <td width="303">Drug Code:</td>
-          <td width="149"><input type="text" name="drugCode" id="drugCode" placeholder="DCNXX-XX-XX" required/></td>
+		  <div>
+          <td><span  style="font-size: 18px">Drug Code:</span></td>
+          <td><p>
+            <input type="text" name="drugCode" id="drugCode" placeholder="DCNXX-XX-XX" required/>
+          </p></td></div>
         </tr>
         <tr>
-          <td>Drug Name:</td>
-          <td><input type="text" name="drugName" id="drugName" required/></td>
+		  <td><span style="font-size: 18px">Drug Name:</span></td>
+          <td><p>
+            <input type="text" name="drugName" id="drugName" required/>
+          </p></td>
         </tr>
         <tr>
-          <td>Price:</td>
-          <td><input type="text" name="price" id="price" placeholder="Ex:500" required/></td>
+          <td><span  style="font-size: 18px">Price:</span></td>
+         <td><p>
+           <input  type="text" name="price" id="price" placeholder="Ex:500" required/>
+         </p></td>
         </tr>
         <tr>
-          <td>Quantity:</td>
-          <td><input type="number" name="quantity" id="quantity" required></td>
+			<td><span  style="font-size: 18px">Quantity:</span></td>
+          <td><input  type="number" name="quantity" id="quantity" required></td>
         </tr>
         <tr>
-          <td height="123" colspan="2"><p>
-            <input type="submit" name="submit" value="Add" onClick="validateAll()"/>
-            <input type="reset" name="reset" value="Reset">
-            <a href="ViewStocks.php">
-            <input type="button" name=ViewStocks value="View" >
+          <td height="123" colspan="2"><p>&nbsp;
+            <input name="submit" type="submit"  onClick="validateAll()" value="Add"/>&nbsp;
+             <input name="reset" type="reset" value="Reset">
+            <a href="ViewStocks.php">&nbsp;
+            <input name=ViewStocks type="button"  value="View" >
             </a></p>
           </td>
         </tr>
       </tbody>
     </table>
-</form>
+  </form>
 </body>
 	
 <script>
