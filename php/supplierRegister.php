6819be66560feec20b2175adefb74b35926d7bea
MadhushaSiriwardane <madhushasiriwardane@gmail.com>
2021-10-29 22:31:59 +0530

    Modified by Madhusha

--- php/supplierRegister.php
+++ php/supplierRegister.php
@@ -1,8 +1,17 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Supplier Registration</title>
+  <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>JayaSiri Pharmacy Supplier Register</title>
+	  
+  <link href="../css/bootstrap-4.3.1.css" rel="stylesheet">
+	<script src="../js/jquery-3.3.1.min.js"></script>
+	<script src="../js/popper.min.js"></script> 
+	<script src="../js/bootstrap-4.3.1.js"></script>
+	
+<link rel="stylesheet" href = "../css/Form.css">
 </head>
 
 <body>
