348061fcbd9170238c58c8a13336cb99f4dbd910
shehaniliyanage <it20760656@my.sliit.lk>
2021-11-16 17:57:40 +0530

    Modified ViewStocks.php

--- php/ViewStocks.php
+++ php/ViewStocks.php
@@ -9,7 +9,6 @@ $sql = "SELECT * FROM stock";
 
 $result = $conn->query($sql);
 
-
 ?>
 
 <!DOCTYPE html>
@@ -51,29 +50,99 @@ $result = $conn->query($sql);
 	<h1>View Stocks</h1>
 	</center>
 	<p><div class="container">
-	  <h4>Filter the stocks details:</h4><p>
-   <input id="myInput" type="text"placeholder="Search.."></p>
-<<<<<<< HEAD
-   <p><p>
-     <button onclick="sortTable()">Sort Table</button></p><br></p>
-<table class="table table-striped"align="center" border="1px" style="width:600px; line-height: 40px;">
-
+  <h4>Filter the stocks details:</h4><p>
+   <input id="myInput" type="text"placeholder="Search Stocks..."></p>
+   <p>&nbsp;</p>
+  <table width="100" border="2px" align="left" class="table table-striped" style="width:600px; line-height: 40px;">
+    <tr>
+		  		<th width="33%">Net Worth of Stocks</th>
+		        <th width="33%">Income without Stock Worth</th>
+		        <th width="33%">Total Stock Quantity</th>
+		  	</tr>
+		    <tr>
+				<?php
+				$sql = "SELECT SUM(price) FROM stock";
+				$results = mysqli_query($conn,$sql);
+				if(mysqli_num_rows($results)>0)
+				{
+					while($row = mysqli_fetch_assoc($results))
+					{
+						$total= $row['SUM(price)'];
+					}
+				?>
+		  		<td height="46"><?php echo $total  ?></td>
+				<?php
+					
+				}
+				mysqli_close($conn);
+				?>
+				<?php
+				$conn = mysqli_connect("localhost","root","","jayasiripharmacydb");
+				if(!$conn)
+				{
+					die("Cannot connect to the database server");
+				}
+				$sql = "SELECT SUM(totalAmount) FROM customerorder";
+				$results = mysqli_query($conn,$sql);
+				if(mysqli_num_rows($results)>0)
+				{
+					while($row = mysqli_fetch_assoc($results))
+					{
+						$income= $row['SUM(totalAmount)'];
+					}
+				?>
+		  		
+				<?php
+					
+				}
+				mysqli_close($conn);
+					?>
+				<td><?php echo $income-$total ?></td>
+				<?php
+				$conn = mysqli_connect("localhost","root","","jayasiripharmacydb");
+				if(!$conn)
+				{
+					die("Cannot connect to the database server");
+				}
+				$sql = "SELECT SUM(quantity) FROM stock";
+				$results = mysqli_query($conn,$sql);
+				if(mysqli_num_rows($results)>0)
+				{
+					while($row = mysqli_fetch_assoc($results))
+					{
+						$qty= $row['SUM(quantity)'];
+					}
+				?>
+			  <td><?php echo $qty ?></td>
+				<?php
+					
+				}
+				mysqli_close($conn);
+				?>
+  	</tr>	</table>
+  <p><br>
+    <br>
+  </p>
+  <p>&nbsp; </p>
   <p>&nbsp;</p>
-   <p>
-  <button class="badge-success" onclick="sortTable()">Sort Table</button></p><br></p>
-<table class="table table-striped"align="center" border="1px" style="width:600px; line-height: 40px;">
-
+  <p>&nbsp;</p>
+  <br>  
+  <p><a href="user_data_print.php" class="btn btn-warning">Generate a Stock Report</a>
+    <button class="btn btn-success" onclick="sortTable();">Sort Table</button>
+  </p>
+  <p>&nbsp;</p>
+  <table width="1112" border="1px"align="center" class="table table-striped" style="width:1050px; line-height: 40px;" >
 	<thead>
 		<tr>
-		<th scope="col">Did</th>
-		<th scope="col" >Drug Code</th>
-		<th scope="col" >Drug Name</th>
-		<th scope="col" >Price</th>
-		<th scope="col" >Quantity</th>
-		<th scope="col" >Action</th>
+		<th width="77" scope="col">Did</th>
+		<th width="125" scope="col" >Drug Code</th>
+		<th width="112" scope="col" >Drug Name</th>
+		<th width="85" scope="col" >Price</th>
+		<th width="123" scope="col" >Quantity</th>
+		<th width="125" scope="col" >Action</th>
 	</tr>
 	</thead>
-	<tbody id="myTable">	
+	<tbody id="myTable" >	
 		<?php
 			if ($result->num_rows > 0) {
 				
@@ -86,7 +155,7 @@ $result = $conn->query($sql);
 					<td><?php echo $row['drugName']; ?></td>
 					<td><?php echo $row['price']; ?></td>
 					<td><?php echo $row['quantity']; ?></td>
-					<td width="141"><p><a  href="UpdateStocks.php?id=<?php echo $row['Did']; ?>"</a><input name=UpdateStocks type="button" class="badge-danger"  value="Update" >&nbsp;<a href="DeleteStocks.php?id=<?php echo $row['Did']; ?>"</a><input name=DeleteStocks type="button" class="badge-primary"  value="Delete" ></p></td>
+					<td width="488"><p><a  href="UpdateStocks.php?id=<?php echo $row['Did']; ?>"</a><input name=UpdateStocks type="button" class="btn btn-danger"  value="Update" >&nbsp;<a href="DeleteStocks.php?id=<?php echo $row['Did']; ?>"</a><input name=DeleteStocks type="button" class="btn btn-primary"  value="Delete" ></p></td>
 					</tr>	
 					
 		<?php		
@@ -94,13 +163,40 @@ $result = $conn->query($sql);
 			}
 		?>
 	        	
-        </tbody>
-   </table>
-	  
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp; </p>
+  </tbody>
+  </table>
 <script>
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("myTable");
+  switching = true;
+  
+  while (switching) {
+    
+    switching = false;
+    rows = table.rows;
+    
+    for (i = 1; i < (rows.length - 1); i++) {
+      
+      shouldSwitch = false;
+      
+      
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
 $(document).ready(function(){
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
@@ -109,30 +205,6 @@ $(document).ready(function(){
     });
   });
 });
-function sortTable() {
-  var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById("myTable");
-  switching = true;
-  while (switching) {
-    switching = false;
-    rows = table.rows;
-    for (i = 1; i < (rows.length - 1); i++) {
-      shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
-      
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-       
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
   </script>
 </body>
 </html>
